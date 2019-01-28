c5f8ebfd7a9c70117bdf92688b8ddadc2d1b1a6b
Tobias Theel <tt@fino.digital>
2019-01-28 17:33:20 +0100

    Added new method to support custom authorization header

--- middleware.go
+++ middleware.go
@@ -12,21 +12,55 @@ import (
 // AuthenticationMiddleWare is used to validate the JWT
 type AuthenticationMiddleWare interface {
 	CheckToken(next echo.HandlerFunc) echo.HandlerFunc
+	CheckTokenCustomHeader(next echo.HandlerFunc) echo.HandlerFunc
 	CheckScope(next echo.HandlerFunc) echo.HandlerFunc
 }
 
 type authenticationMiddleWare struct {
-	gocloak      gocloak.GoCloak
-	realm        string
-	allowedScope string
+	gocloak          gocloak.GoCloak
+	realm            string
+	allowedScope     string
+	customHeaderName *string
 }
 
 // NewAuthenticationMiddleWare instantiates a new AuthenticationMiddleWare
-func NewAuthenticationMiddleWare(gocloak gocloak.GoCloak, realm, allowedScope string) AuthenticationMiddleWare {
+func NewAuthenticationMiddleWare(gocloak gocloak.GoCloak, realm, allowedScope string, customHeaderName *string) AuthenticationMiddleWare {
 	return &authenticationMiddleWare{
-		gocloak:      gocloak,
-		realm:        realm,
-		allowedScope: allowedScope,
+		gocloak:          gocloak,
+		realm:            realm,
+		allowedScope:     allowedScope,
+		customHeaderName: customHeaderName,
+	}
+}
+
+// CheckTokenCustomHeader used to verify authorization tokens
+func (auth *authenticationMiddleWare) CheckTokenCustomHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		token := c.Request().Header.Get(*auth.customHeaderName)
+		if token == "" {
+			return c.JSON(http.StatusUnauthorized, gocloak.APIError{
+				Code:    403,
+				Message: "Authorization header missing",
+			})
+		}
+
+		token = strings.Replace(token, "Bearer ", "", 1)
+		decodedToken, _, err := auth.gocloak.DecodeAccessTokenCustomClaims(token, auth.realm)
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, gocloak.APIError{
+				Code:    403,
+				Message: "Invalid or malformed token",
+			})
+		}
+
+		if !decodedToken.Valid {
+			return c.JSON(http.StatusForbidden, gocloak.APIError{
+				Code:    http.StatusForbidden,
+				Message: "Invalid Token",
+			})
+		}
+
+		return next(c)
 	}
 }
 
